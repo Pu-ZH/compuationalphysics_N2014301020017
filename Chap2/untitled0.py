5c149385542bb20475439a47456b4a336f7fab32
Pu-ZH <756728756@qq.com>
2016-09-17 17:25:04 +0800

    Delete untitled0.py

--- Chap2/untitled0.py
+++
@@ -1,9 +0,0 @@
-
-print 'aaaa       aaaaaaa    aa   aa'
-print 'aa  aa     aaaaaaa    aa   aa'
-print 'aa   aa         aa    aa   aa'
-print 'aa  aa        aa      aaaaaaa'
-print 'aaa         aa        aaaaaaa'
-print 'aa         aa         aa   aa'
-print 'aa         aaaaaaa    aa   aa'
-print 'aa         aaaaaaa    aa   aa'
