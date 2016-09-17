ee5a1ed20fd16cc68e082784f051b2bb7858aeb8
Pu-ZH <756728756@qq.com>
2016-09-17 16:58:33 +0800

    Add files via upload

---
+++ Chap2/untitled0.py
@@ -0,0 +1,9 @@
+
+print 'aaaa       aaaaaaa    aa   aa'
+print 'aa  aa     aaaaaaa    aa   aa'
+print 'aa   aa         aa    aa   aa'
+print 'aa  aa        aa      aaaaaaa'
+print 'aaa         aa        aaaaaaa'
+print 'aa         aa         aa   aa'
+print 'aa         aaaaaaa    aa   aa'
+print 'aa         aaaaaaa    aa   aa'
